490d5725c1ea9bd8e13007b919bb45ef206c7945
David <davejs136@gmail.com>
2019-12-09 11:55:46 -0400

    First commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,81 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+  // Query for nodes to use in creating pages.
+  // Consultas para los nodos que se usaran en la creacion de paginas
+
+  resolve(
+    graphql(request)
+      .then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        return result;
+      })
+  )
+});
+
+// Implement the Gatsby API "createPages". This is called once the
+// data layer is boostrapped to let plugins create pages from data.
+// Implementa el api de Gatsby "createPages". Este es llamado una vez
+// que el dato es accedido por el plugin para crear las paginas desde los datos
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const getArticles = makeRequest(graphql, `
+  {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+  `).then(result => {
+    // Creates pages for each article
+    // Crea la pagina por cada articulo
+    result.data.allStrapiArticle.edges.forEach(({node}) => {
+      createPage({
+        path: `/${node.id}`,
+        component: path.resolve(`src/templates/article.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  });
+
+  const getAuthors = makeRequest(graphql, `
+  {
+    allStrapiUser {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+  `).then(result => {
+    // Create pages for each user
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id
+        }
+      })
+    })
+  })
+
+  // Query for articles nodes to user in creating pages
+  // Consulta para los nodos de articulos para usar en la creacion de paginas
+  return ([
+    getArticles,
+    getAuthors,
+  ]);
+}
